368eca3c9e2a67a1d36ade2380f596e1ef382009
JuliuChe <chevalleyjulien2@gmail.com>
2021-12-20 22:07:20 +0100

    Design_Blocs_DirSpeedetSafety

---
+++ Cursor_test/hdl/Control_DIR_SPEED_tester_test_DIR.vhd
@@ -0,0 +1,13 @@
+--
+-- VHDL Architecture Cursor_test.Control_DIR_SPEED_tester.test_DIR
+--
+-- Created:
+--          by - Julie.UNKNOWN (LAPTOP-J400VU4F)
+--          at - 21:53:55 20.12.2021
+--
+-- using Mentor Graphics HDL Designer(TM) 2019.2 (Build 5)
+--
+ARCHITECTURE test_DIR OF Control_DIR_SPEED_tester IS
+BEGIN
+END ARCHITECTURE test_DIR;
+
---
+++ Cursor_test/hdl/Safety_tester_test_safety.vhd
@@ -0,0 +1,13 @@
+--
+-- VHDL Architecture Cursor_test.Safety_tester.test_safety
+--
+-- Created:
+--          by - Julie.UNKNOWN (LAPTOP-J400VU4F)
+--          at - 21:52:41 20.12.2021
+--
+-- using Mentor Graphics HDL Designer(TM) 2019.2 (Build 5)
+--
+ARCHITECTURE test_safety OF Safety_tester IS
+BEGIN
+END ARCHITECTURE test_safety;
+
